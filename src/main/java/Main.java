ea19c127a9706e1ca44fdf552583e3902ee74747
Rus, Renato <Renato.Rus@garmin.com>
2019-08-20 16:21:00 +0300

    Added fxml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import service.PolynomialService;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application {
+ /*   public static void main(String[] args) {
         System.out.println(Integer.parseInt("4"));
         PolynomialService polynomialService = new PolynomialService();
         System.out.println(polynomialService.inputToPolynomialForm("4x^4 + 5x^3 - 3x^2 + 2x - 1"));
@@ -13,5 +19,18 @@ public class Main {
         System.out.println("Integration : " + polynomialService.integration(polynomialService.inputToPolynomialForm("3x^3 - 2x^2 + 3x + 4")).integrationSpecialRepresentation());
         System.out.println("Degree : " + polynomialService.inputToPolynomialForm("3x^3 + 2x^2 + 3x + 1").getPolynomialDegree());
      //   System.out.println("Division : " + polynomialService.division(polynomialService.inputToPolynomialForm("3x^3 - 6x^2 + 2x + 8"), polynomialService.inputToPolynomialForm("2x + 3")));
+    }*/
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       // BorderPane root = new BorderPane();
+      //  Scene scene = new Scene(root,400,400);
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/test.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
